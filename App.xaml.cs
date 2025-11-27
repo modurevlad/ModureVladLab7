a4cb667ab76586ca75cf1f927777e75a32027195
modurevlad <modure_vlad@yahoo.com>
2025-11-27 15:12:43 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,26 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using ModureVladLab7.Data;
+using System;
+using System.IO;
 
 namespace ModureVladLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
