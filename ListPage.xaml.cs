30024d3ab68fd6da57f5702c384ffa7418dbcc28
modurevlad <modure_vlad@yahoo.com>
2025-11-30 10:55:44 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -8,6 +8,8 @@ public partial class ListPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -15,10 +17,52 @@ public partial class ListPage : ContentPage
         await App.Database.SaveShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
+    }
+
+    async void OnDeleteItemClicked(object sender, EventArgs e)
+    {
+        var shopl = (ShopList)BindingContext;
+        var selectedProduct = listView.SelectedItem as Product;
+        if (selectedProduct == null)
+        {
+            return;
+        }
+         
+        var listProducts = await App.Database.GetListProductsRawAsync(shopl.ID);
+        var lp = listProducts.FirstOrDefault(x => x.ProductID == selectedProduct.ID);
+
+        if (lp != null)
+        {
+            await App.Database.DeleteListProductAsync(lp);
+        }
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
+
+
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
     }
 }
